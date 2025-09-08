5da88c8153a3e90d403850f65075ddf41ea2cfd8
mvanberk <mvanberk@protonmail.com>
2025-09-07 21:07:00 -0400

    Assignment 1 - 7 Sep 25

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //task 1
+        Scanner sc = new Scanner(System.in);
+
+        //task 2
+        System.out.println("How old are you? ");
+        String age = sc.nextLine();
+        System.out.println("Damn, you're " + age + "?!");
+
+        //task 3
+        int num1 = 10;
+        int num2 = 37;
+        System.out.println(num1 + num2);
+
+        //What I learned: it's easy enough to do this while looking at sample code, but I'm still having a hard time remembering the exact syntax for Java. Python was much simpler. I understand what certain lines do, but I don't fully understand the components of those lines so it feels like I don't understand the code as well.
+    }
+}
