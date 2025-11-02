080e192a9f84ea087e4f504f0cbc61e4f9af0a92
mvanberk <mvanberk@protonmail.com>
2025-11-02 10:33:05 -0500

    Update Enemy.java

--- Assignment 7/Enemy.java
+++ Assignment 7/Enemy.java
@@ -11,14 +11,14 @@ public class Enemy {
     }
 
     void attack(Enemy target) {
-        if (target.health <= 0) {
+        if (target.health <= 0) { //checks if target is already dead
             System.out.println(name + " tries to attack " + target.name + " but " + target.name + " is already dead.");
             System.out.println();
             return;
         }
-        System.out.println(name + " inflicts " + damage + " damage to " + target.name + "!");
+        System.out.println(name + " inflicts " + damage + " damage to " + target.name + "!"); //inflict damage
         target.health = target.health - damage;
-        if (target.health <= 0) {
+        if (target.health <= 0) { //checks if attack killed target
             System.out.println(target.name + " has been killed!");
         }
         else {
